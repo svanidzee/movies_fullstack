9a91ffb11ac728a05c3fdcc726ec55fc1174e5d0
svanidzee <svanidzeee@gmail.com>
2023-03-24 16:26:57 +0300

    create basic movies route

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import movies from './api/movies.route.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('*', (req, res) => {
-  res.status(200).json({ data: 1212 });
-});
-
+app.use('/api/v1/movies', movies);
 app.use('*', (req, res) => {
   res.status(404).json({ error: 'not found' });
 });
