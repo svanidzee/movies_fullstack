fb73a6fd4fc65f9e5895d4aa674a78bc3a929cf4
svanidzee <svanidzeee@gmail.com>
2023-03-24 18:11:58 +0300

    review update

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 router.route('/').get(MoviesController.apiGetMovies);
 
-router.route('/review').post(ReviewsController.apiPostReview);
+router
+  .route('/review')
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview);
 
 export default router;
--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -25,4 +25,35 @@ export default class ReviewsController {
       res.status(500).json({ error: e.message });
     }
   }
+
+  static async apiUpdateReview(req, res) {
+    try {
+      const reviewId = req.body.review_id;
+      const review = req.body.review;
+
+      const date = new Date();
+
+      const ReviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        req.body.user_id,
+        review,
+        date,
+      );
+      console.log(ReviewResponse);
+
+      var { error } = ReviewResponse;
+      if (error) {
+        res.status.json({ error });
+      }
+
+      if (ReviewResponse.modifiedCount === 0) {
+        throw new Error(
+          'unable to update review. User may not be original poster',
+        );
+      }
+      res.json({ status: 'success ' });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
 }
