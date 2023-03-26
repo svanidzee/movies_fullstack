32bb060dcf5bfa317fbe30e0a6ad1dd2507a1c8e
svanidzee <svanidzeee@gmail.com>
2023-03-26 21:53:09 +0300

    change url: localhost to deployed url

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { initMovies, initMovie } from '../api';
 
 export default function useApi() {
-  const baseURL = 'http://localhost:5000/api/v1';
+  const baseURL = 'https://mernback-4nnr.onrender.com/api/v1';
 
   return {
     getMovies: initMovies(baseURL),
